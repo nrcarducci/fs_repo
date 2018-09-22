494bc0a8faec50087f0bfb485b33f41f66e2237e
neil carducci <nrcarducci@gmail.com>
2018-09-22 14:20:28 -0700

    adding testing.js

---
+++ testing.js
@@ -0,0 +1,12 @@
+function didItpush(input)
+{
+    if(input==="yes"||input==="ya"||input ==="sure did")
+        return true;
+    else
+        return false;
+}
+
+console.log(didItpush("yes"));
+console.log(didItpush("ya"));
+console.log(didItpush("sure did"));
+console.log(didItpush("naw"));//jk it totally did ;)
