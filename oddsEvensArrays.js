97ef38287562505ded3215cff912f4fb9aba9560
nrcarducci <nrcarducci@gmail.com>
2018-10-05 13:11:09 -0700

    newExamples

--- oddsEvensArrays.js
+++ oddsEvensArrays.js
@@ -1,5 +1,10 @@
 let array = [0,1,2,3,4,5,6,7,8,9,];
 
+function reducer(i,curr)
+{
+    return i+curr;
+} 
+
 function odd(x)
 {
     if(x%2===1)
@@ -17,4 +22,16 @@ function oddsAndEvens(arr,func1,func2)
     return [arr.filter(func1),arr.filter(func2)];
 }
 
-console.log(oddsAndEvens(array,even,odd));
+let sperateArr = oddsAndEvens(array,even,odd);
+
+const mappedArr=sperateArr.map(x=>x);
+
+console.log(mappedArr);
+
+const reduceArr=[];
+sperateArr.forEach(element => 
+{
+    reduceArr.push((element.reduce(reducer)));
+});
+
+console.log(reduceArr);
